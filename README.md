be74dd78e138bea53684737163238f3010a8b2ce
DmitryKrinitsyn <dmitry.krinitsyn@gmail.com>
2024-01-15 12:32:12 +0100

    readme more details

--- README.md
+++ README.md
@@ -4,14 +4,34 @@
 Common
 
 - sudo port install ripgrep
-
 - zsh
 
 ---
 Iterm2
 
+1. color scheme
+https://raw.githubusercontent.com
+2. oh my zsh
+https://github.com/ohmyzsh/ohmyzsh
+3. fonts
+https://www.nerdfonts.com/font-downloads
+meslo
+4. ZSH_THEME="zhann"
+5. syntax highlighting 
+https://github.com/zsh-users/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+6. autosuggestion
+https://github.com/zsh-users/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+7. edit ./zshrc -> plugins zsh-syntax-highlighting zsh-autosuggestions
+
 ---
 Tmux
 
+Prefix + I/U - intall/update plugins
+
 ---
 NeoVim
+
+put "nvim" folder in ~/.config
---
+++ nvim/init.lua
@@ -0,0 +1,57 @@
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.g.mapleader = " "
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+          "nvim-lua/plenary.nvim",
+          "nvim-tree/nvim-web-devicons",
+          "MunifTanjim/nui.nvim",
+        }
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    },
+    {'akinsho/toggleterm.nvim', version = "*", config = true}
+}
+
+
+require("lazy").setup(plugins)
+
+local builtin = require ("telescope.builtin")
+vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>t', '<Cmd>Neotree toggle<CR>')
+vim.keymap.set('n', '<leader>c', '<Cmd>ToggleTerm direction=float<CR>')
+
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin-latte"
+
+require('lualine').setup()
+require('toggleterm').setup()
