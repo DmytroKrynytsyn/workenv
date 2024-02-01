84cc696df2b60e9affe09e4614c57619bf7078dd
DmytroKrynytsyn <dmitry.krinitsyn@gmail.com>
2024-02-01 10:52:22 +0100

    some keymap is added

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
         config = function()
             local builtin = require ("telescope.builtin")
             vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
         end
     },
     {
