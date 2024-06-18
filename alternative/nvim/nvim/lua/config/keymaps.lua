a1e2c7c4f3ba26b7abce2374b86efb74b264d2bd
DmytroKrynytsyn <dmitry.krinitsyn@gmail.com>
2024-06-18 19:41:57 +0200

    alternative nvim config, keys

--- alternative/nvim/nvim/lua/config/keymaps.lua
+++ alternative/nvim/nvim/lua/config/keymaps.lua
@@ -70,6 +70,7 @@ function Telescope ()
   vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[F]ind current [W]ord' })
   vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
   vim.keymap.set('n', '<leader>fd', builtin.diagnostics, { desc = '[F]ind [D]iagnostics' })
+  vim.keymap.set('n', '<leader>fr', builtin.registers, { desc = '[F]ind [R]egisters' })
   vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = '[F]ind Recent Files ("." for repeat)' })
   vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
