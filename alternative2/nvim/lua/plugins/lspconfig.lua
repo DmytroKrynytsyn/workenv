896f8b8e8a51ef2ffa4c5613adaab5fd952bc414
DmytroKrynytsyn <dmitry.krinitsyn@gmail.com>
2024-10-01 22:55:08 +0200

    completions

--- alternative2/nvim/lua/plugins/lspconfig.lua
+++ alternative2/nvim/lua/plugins/lspconfig.lua
@@ -1,8 +1,15 @@
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({})
-        lspconfig.pyright.setup({})
-    end
+	"neovim/nvim-lspconfig",
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		local lspconfig = require("lspconfig")
+		lspconfig.lua_ls.setup({ capabilities = capabilities })
+		lspconfig.pyright.setup({ capabilities = capabilities })
+
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+		vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, {})
+		vim.keymap.set("n", "<leader>cr", vim.lsp.buf.references, {})
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+	end,
 }
