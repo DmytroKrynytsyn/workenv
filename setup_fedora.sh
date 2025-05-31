519ddb1f1da8d297b2082191247b40ee50e91717
DmytroKrynytsyn <dmitry.krinitsyn@gmail.com>
2025-05-31 22:32:27 +0200

    more fedora script

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+echo common
+
+dnf upgrade -y --refresh
 
 echo utilities
 
-dnf install -y ripgrep zsh fzf
+dnf install -y ripgrep zsh fzf kitty tmux
 
 
 
 echo kitty
 
-dnf upgrade --refresh
-dnf install -y kitty
 kitten themes alabaster
+
+echo "font_size 14" >> ~/.config/kitty/kitty.conf
 
 
 echo tmux
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+
+echo config
+
+echo "options hid_apple swap_opt_cmd=1 fnmode=2" > /etc/modprobe.d/hid_apple.conf
