9a03e26eaa2c6f707602a6ac54f0ab40ca03c62b
DmytroKrynytsyn <dmitry.krinitsyn@gmail.com>
2025-05-31 23:00:19 +0200

    more fedora setup

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -4,10 +4,15 @@ echo common
 
 dnf upgrade -y --refresh
 
+
 echo utilities
 
-dnf install -y ripgrep zsh fzf kitty tmux
-
+dnf install -y \
+    ripgrep \
+    zsh \
+    fzf \
+    kitty \
+    tmux
 
 
 echo kitty
@@ -20,8 +25,15 @@ echo "font_size 14" >> ~/.config/kitty/kitty.conf
 echo tmux
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp tmux/.tmux.conf ~/
 
 
 echo config
 
 echo "options hid_apple swap_opt_cmd=1 fnmode=2" > /etc/modprobe.d/hid_apple.conf
+
+fzf --zsh > ~/.config/fzf_integration.sh
+echo "source ~/.config/fzf_integration.sh" >> .zshrc
+
+cp -r texteditornvim/nvim ~/.config/
+
